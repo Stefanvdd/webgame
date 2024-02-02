6e63bbc9f5871d49ed4629ae4d4fd5b2e8de4fcb
Stefanvdd <147039378+Stefanvdd@users.noreply.github.com>
2024-02-02 10:15:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,168 @@
+// Selectoren voor verschillende elementen in de HTML
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
+
+// Toevoegen van een klikgebeurtenis aan de resetknop
+let resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', test);
+
+// Een eenvoudige functie die wordt uitgevoerd wanneer de resetknop wordt geklikt
+function test() {
+    console.log("test reset button");
+}
+
+// De toestand van het spel
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+
+// Een hulpmiddel om een array willekeurig te schudden
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    for (let i = clonedArray.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1))
+        const original = clonedArray[i]
+
+        clonedArray[i] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
+
+// Een functie om willekeurige items uit een array te selecteren
+const pickRandom = (array, items) => {
+    const clonedArray = [...array]
+    const randomPicks = []
+
+    for (let i = 0; i < items; i++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
+
+// Genereren van het spelbord
+const generateGame = () => {
+    const dimensions = selectors.board.getAttribute('data-dimension')
+
+    if (dimensions % 2 !== 0) {
+        throw new Error("The dimension of the board must be an even number.")
+    }
+
+    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2)
+    const items = shuffle([...picks, ...picks])
+    const cards = `
+        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `
+
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    selectors.board.replaceWith(parser.querySelector('.board'))
+}
+
+// Starten van het spel
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `Time: ${state.totalTime} sec`
+    }, 1000)
+}
+
+// Omdraaien van niet-gematchte kaarten
+const flipBackCards = () => {
+    document.querySelectorAll('.card:not(.matched)').forEach(card => {
+        card.classList.remove('flipped')
+    })
+
+    state.flippedCards = 0
+}
+
+// Omdraaien van een kaart en controleren op overeenkomst
+const flipCard = card => {
+    state.flippedCards++
+    state.totalFlips++
+
+    if (!state.gameStarted) {
+        startGame()
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped')
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched')
+            flippedCards[1].classList.add('matched')
+        }
+
+        setTimeout(() => {
+            flipBackCards()
+        }, 1000)
+    }
+
+    // Controleren op winnende staat
+    if (!document.querySelectorAll('.card:not(.flipped)').length) {
+        setTimeout(() => {
+            selectors.boardContainer.classList.add('flipped')
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    You won!<br />
+                    with <span class="highlight">${state.totalFlips}</span> moves<br />
+                    under <span class="highlight">${state.totalTime}</span> seconds
+                </span>
+            `
+
+            clearInterval(state.loop)
+        }, 1000)
+    }
+}
+
+// Het toewijzen van klikgebeurtenissen aan de kaarten en de startknop
+const attachEventListeners = () => {
+    document.addEventListener('click', event => {
+        const eventTarget = event.target
+        const eventParent = eventTarget.parentElement
+
+        // Controle op het klikken van een kaart of de startknop
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent)
+        } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+            startGame()
+        }
+    })
+}
+
+// Genereren van het spelbord en toewijzen van gebeurtenissen
+generateGame()
+attachEventListeners()
